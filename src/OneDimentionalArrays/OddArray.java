ff2e42f29c95da603feee8cbc4ff9b2d70092815
dbobkova <das-bobkova@yandex.ru>
2016-10-31 17:16:29 +0400

    OddArray class - done

--- src/OneDimentionalArrays/OddArray.java
+++ src/OneDimentionalArrays/OddArray.java
@@ -4,7 +4,21 @@ package OneDimentionalArrays;
  * Created by dbobkova on 31.10.2016.
  */
 public class OddArray {
-
+    public static void oddArrayCreator() {
+        int N = 100/2; //numbers of odd values
+        int[] array = new int[N];
+        array[0] = 1;
+        for (int i = 1; i < N; i++){
+            array[i] = array[i-1] + 2;
+        }
+        for (int el:array ) {
+            System.out.print(el + " ");
+        }
+        System.out.println();
+        for (int i = N-1; i>= 0; i-- ){
+            System.out.print(array[i] + " ");
+        }
+    }
 
 
 }
