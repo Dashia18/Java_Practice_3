3fd97ff0e5fdaf9619391370374c2c43ebd82eb5
dbobkova <das-bobkova@yandex.ru>
2016-11-01 15:36:55 +0400

    Made a title for number of tasks 1-3

--- src/Runner.java
+++ src/Runner.java
@@ -8,15 +8,15 @@ public class Runner {
     public static void main(String[] args) {
 
      //Sorting (3 types) array and time measurement
-        System.out.println("Sorting (3 types) array and time measurement  \n" );
+        System.out.println("Sorting (3 types) array and time measurement/Task 1  \n" );
         Sorting.sortingPerform();
 
      //Factorial (2 way) and time measurement
-        System.out.println("\n\nFactorial (2 ways) and time measurement  \n" );
+        System.out.println("\n\nFactorial (2 ways) and time measurement/Task 2  \n" );
         Factorial.factorialPerform();
 
       //Figure plot #
-        System.out.println("\n\nFigure ploter: \n");
+        System.out.println("\n\nFigure ploter/Task 3: \n");
         FigurePlotter.figurePlotterPerform();
 
 
