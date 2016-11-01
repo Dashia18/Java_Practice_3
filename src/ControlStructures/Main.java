ca7c83ddac5b2ca37bfa8020e7925c5a27124356
Dashia <das-bobkova@yandex.ru>
2016-11-01 23:03:07 +0300

    Task 6 about time measurement of control structures (if...else/ switch...case) - done!

--- src/ControlStructures/Main.java
+++ src/ControlStructures/Main.java
@@ -10,19 +10,65 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Enum time measurement
+
+
+    //Enum time measurement
         System.out.println("Enum time measurement/ Task 6");
         Animals dog = Animals.Dog;
         getAnimalSoundIF(dog);
-        getAnimalSoundCASE(dog);
+        getAnimalSoundSWITCH(dog);
 
         Random rand = new Random();
-        int[] array = new int[50];
-        for (int i = 0; i  < 50; i++)
+        int N = 50;
+        int[] array = new int[N];
+        for (int i = 0; i  < N; i++)
         {
-            array[i] = rand.nextInt(10);
+            array[i] = rand.nextInt(11);
             System.out.print(array[i] + " ");
         }
+        System.out.println();
+
+
+    //Array time measurement
+        System.out.println("IF  - ARRAY");
+        int[] countIf = new int[11];
+        //time measurement
+        long startTimeNumberIF = System.nanoTime();
+        for (int i = 0; i  < N; i++)
+        {
+            getNumberIF(array[i], countIf);
+        }
+        long estimatedTimeNumberIF = System.nanoTime() - startTimeNumberIF;
+
+        System.out.println("SWITCH - ARRAY");
+        int[] countSwitch = new int[11];
+        //time measurement
+        long startTimeNumberSWITCH = System.nanoTime();
+        for (int i = 0; i  < N; i++)
+        {
+            getNumberSWITCH(array[i], countSwitch);
+        }
+        long estimatedTimeNumberSWITCH = System.nanoTime() - startTimeNumberSWITCH;
+
+
+
+
+        System.out.println("Count for each values [0,10] SWITCH");
+        for (int i = 0; i < 11; i++){
+            System.out.print(countSwitch[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Count for each values [0,10] IF");
+        for (int i = 0; i < 11; i++){
+            System.out.print(countIf[i] + " ");
+        }
+
+
+        //Comparison
+        System.out.println();
+        System.out.println("estimatedTimeIF (1 operation):" + estimatedTimeNumberIF/N);
+        System.out.println("estimatedTimeSWITCH (1 operation):" + estimatedTimeNumberSWITCH/N);
+        System.out.println();
 
 
 
@@ -55,8 +101,8 @@ public class Main {
         System.out.println("estimatedTimeIF:" + estimatedTimeIF);
         System.out.println();
     }
-    public static void getAnimalSoundCASE(Animals a){
-        long startTimeCASE = System.nanoTime();
+    public static void getAnimalSoundSWITCH(Animals a){
+        long startTimeSWITCH = System.nanoTime();
         switch (a){
             case Cat:{System.out.println("Cat say mayu"); break;}
             case Dog:{System.out.println("Dog say gaf"); break;}
@@ -65,12 +111,78 @@ public class Main {
             case Bird:{System.out.println("Bird say kuku-kuku"); break;}
             default:{System.out.println("This animal is mute"); break;}
         }
-        long estimatedTimeCASE = System.nanoTime() - startTimeCASE;
-        System.out.println("estimatedCASE:" + estimatedTimeCASE);
+        long estimatedTimeSWITCH = System.nanoTime() - startTimeSWITCH;
+        System.out.println("estimatedSWITCH:" + estimatedTimeSWITCH);
         System.out.println();
     }
 
 
+    public static void getNumberIF(int n, int[] countIf){
+
+        if(n==0){
+            System.out.println("this is 0");
+            countIf[0]++;
+        }
+        else if(n==1){
+            System.out.println("this is 1");
+            countIf[1]++;
+        }
+        else if(n==2){
+            System.out.println("this is 2");
+            countIf[2]++;
+        }
+        else if(n==3){
+            System.out.println("this is 3");
+            countIf[3]++;
+        }
+        else if(n==4){
+            System.out.println("this is 4");
+            countIf[4]++;
+        }
+        else if(n==5){
+            System.out.println("this is 5");
+            countIf[5]++;
+        }
+        else if(n==6){
+            System.out.println("this is 6");
+            countIf[6]++;
+        }
+        else if(n==7){
+            System.out.println("this is 7");
+            countIf[7]++;
+        }
+        else if(n==8){
+            System.out.println("this is 8");
+            countIf[8]++;
+        }
+        else if(n==9){
+            System.out.println("this is 9");
+            countIf[9]++;
+        }
+        else if(n==10){
+            System.out.println("this is 10");
+            countIf[10]++;
+        }
 
 
+
+    }
+    public static void getNumberSWITCH(int n, int[] countSwitch){
+
+        switch (n)
+        {
+            case 0:{System.out.println("this is 0");countSwitch[0]++;break;}
+            case 1:{System.out.println("this is 1");countSwitch[1]++;break;}
+            case 2:{System.out.println("this is 2");countSwitch[2]++;break;}
+            case 3:{System.out.println("this is 3");countSwitch[3]++;break;}
+            case 4:{System.out.println("this is 4");countSwitch[4]++;break;}
+            case 5:{System.out.println("this is 5");countSwitch[5]++;break;}
+            case 6:{System.out.println("this is 6");countSwitch[6]++;break;}
+            case 7:{System.out.println("this is 7");countSwitch[7]++;break;}
+            case 8:{System.out.println("this is 8");countSwitch[8]++;break;}
+            case 9:{System.out.println("this is 9");countSwitch[9]++;break;}
+            case 10:{System.out.println("this is 10");countSwitch[10]++;break;}
+        }
+
+    }
 }
